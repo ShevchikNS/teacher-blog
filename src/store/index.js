c4b636db6805e8b82b991a54e544a3a56ab9b5c3
ABC Man <Shevchikkola@gmail.com>
2022-10-13 13:58:15 +0300

    add styles

--- src/store/index.js
+++ src/store/index.js
@@ -4,10 +4,12 @@ import thunk from "redux-thunk";
 import {authReducer} from "./authReducer";
 import {userReducer} from "./userReducer";
 import {testReducer} from "./testReducer";
+import {materialReducer} from "./materialReducer";
 
 const rootReducer = combineReducers({
     auth: authReducer,
     currentUser: userReducer,
     tests: testReducer,
+    materials: materialReducer
 })
 export const store  = createStore(rootReducer, composeWithDevTools(applyMiddleware(thunk)))
