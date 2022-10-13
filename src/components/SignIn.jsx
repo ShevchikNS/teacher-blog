75a75b71a48f700af2b715e08314ac1dfa249824
ABC Man <Shevchikkola@gmail.com>
2022-10-13 14:33:20 +0300

    fix

--- src/components/SignIn.jsx
+++ src/components/SignIn.jsx
@@ -10,7 +10,6 @@ import LockOutlinedIcon from '@mui/icons-material/LockOutlined';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import {createTheme, ThemeProvider} from '@mui/material/styles';
-import {changeAuthAction} from "../store/authReducer";
 import {useDispatch} from "react-redux";
 import {useNavigate} from "react-router-dom";
 import {useState} from "react";
